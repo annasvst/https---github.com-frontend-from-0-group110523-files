98b7b1a802956f931847b4f6759b5269cb9dc79b
Anna S. A <anna.atalay@minna.tech>
2023-11-14 20:56:17 +0100

    Add Lesson23 starter files

---
+++ Lesson23/app.js
@@ -0,0 +1,43 @@
+// Events to check that page is loaded (should be applied to window object):
+// DOMContentLoaded - DOM fully loaded
+// load - all elements (including images and styles) are loaded
+window.addEventListener('load',function(){
+  // Select all images
+  // Select previous and next buttons
+  // Select indicators
+
+
+  // Create application state to keep track of the current index
+
+  // Show images function
+  function showImage(index) {
+    // Remove active class from all images
+    // For every element in images array call a function that removes "active" class
+  
+
+    // Remove active class from all indicators
+    // Use loop to loop through all element of array
+
+    // Add active class to the current image and indicator
+    // images[]
+  }
+
+  // handle next click
+  function showNextImage() {
+    // increase currentIndex. What happens if the current index is > than number of images that we have?
+  }
+
+  // handle previous click
+  function showPrevImage() {
+    // [image1, image2, image3]
+    // decrease currentIndex. What happens if the current index is < 0?
+    // if else condition
+  }
+
+  // Add event listeneters for button clicks
+
+
+  // Switch images after 3 seconds automatically using setInterval function
+  // Use 3000 value for 3 second delay
+
+});
