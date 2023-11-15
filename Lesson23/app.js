94ab566f5d7739eed1952fcf536d51d4910bda44
Anna S. A <anna.atalay@minna.tech>
2023-11-15 20:02:37 +0100

    Add lesson 23 completed files

--- Lesson23/app.js
+++ Lesson23/app.js
@@ -5,26 +5,44 @@ window.addEventListener('load',function(){
   // Select all images
   // Select previous and next buttons
   // Select indicators
-
-
+  const allImages = document.querySelectorAll("img");
+  const prevButton = document.getElementById("prev-btn");
+  const nextButton = document.getElementById("next-btn");
+  const allIndicators = document.querySelectorAll(".indicator");
   // Create application state to keep track of the current index
+  let index = 0;
 
   // Show images function
-  function showImage(index) {
+  function showImage() {
+    const activeClass = 'active';
     // Remove active class from all images
     // For every element in images array call a function that removes "active" class
-  
-
+    for (let i = 0; i < allImages.length; i++) {
+      allImages[i].classList.remove(activeClass);
+    }
     // Remove active class from all indicators
     // Use loop to loop through all element of array
+    for(let i = 0; i < allIndicators.length; i++) {
+      allIndicators[i].classList.remove(activeClass);
+    }
 
     // Add active class to the current image and indicator
+    allImages[index].classList.add(activeClass);
+    allIndicators[index].classList.add(activeClass);
     // images[]
   }
 
   // handle next click
   function showNextImage() {
     // increase currentIndex. What happens if the current index is > than number of images that we have?
+  //  0 < index < 3
+    if (0 <= index && index < allImages.length - 1 ){
+      index++;
+    } else {
+      index = 0;
+    }
+    console.log(index);
+    showImage();
   }
 
   // handle previous click
@@ -32,12 +50,31 @@ window.addEventListener('load',function(){
     // [image1, image2, image3]
     // decrease currentIndex. What happens if the current index is < 0?
     // if else condition
+    if (0 < index && index < allImages.length ){
+      index--;
+      console.log(index);
+    } else {
+      index = allImages.length - 1;
+      console.log(index);
+    }
+    showImage();
   }
 
-  // Add event listeneters for button clicks
+  // IIFE - if you need to run a function only once immediately
+  (function sayHello(name) {
+    console.log('Hello ' + name);
+  })();
 
+  // Add event listeneters for button clicks
+  prevButton.addEventListener("click", showPrevImage);
+  // If you need to pass a parameter to your function, use arrow function or anonymus function
+  // prevButton.addEventListener("click", () => sayHello('John'));
+  // prevButton.addEventListener("click", function () {
+  //   sayHello('Jane');
+  // });
+  nextButton.addEventListener("click", showNextImage);
 
   // Switch images after 3 seconds automatically using setInterval function
-  // Use 3000 value for 3 second delay
-
+  // Use 3000 (3 x 1000 milisecond) value for 3 second delay
+  setInterval(showNextImage, 3000);
 });
