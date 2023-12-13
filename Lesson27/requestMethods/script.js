8fc8c8196021f2326d3d04a17d943d9fa2c499e1
Anna S. A <anna.atalay@minna.tech>
2023-12-13 20:10:49 +0100

    Add Lesson27 files

--- Lesson27/requestMethods/script.js
+++ Lesson27/requestMethods/script.js
@@ -9,24 +9,90 @@ HTTP status codes are three-digit numbers that the server sends in response to a
 100... - Informational Responses
 200... - Successful Responses (200 OK, 201 Created, 204 No content)
 300.. - redirection (301 Moved Permanently, Found (or 307 Temporary Redirect))
-400... - Errors (400 Bad Request, 401 Unauthorized, 403 Forbidden, 404 Not Found)
+400... - Errors (
+	400 Bad Request - something is wrong in your request,
+	401 Unauthorized - you don't have access to the resource (API key missing or does not have access to that endpoint),
+	403 Forbidden - you are not allowed to access this enpoint,
+	404 Not Found - you're trying to access a non-existing endpoint (could be used for security reasons as well to hide some resources)
+	)
 500... - Service error (500 Internal Server Error, 502 Bad Gateway, 503 Service Unavailable)
 */
+
+// Mocked - it behaves like if the request was processed even if it is not
 
 const URL = 'https://jsonplaceholder.typicode.com/posts';
 
 function getPosts() {
+	fetch('https://jsonplaceholder.typicode.com/posts')
+		.then((response) => response.json())
+		.then((data) =>
+			data.map((item) => {
+				//  post-container
+				const post = document.createElement('div');
+				post.classList.add('post');
+				const postTitle = document.createElement('h2');
+				postTitle.innerText = item.title;
+				post.classList.add('post-title');
+
+				const postBody = document.createElement('p');
+				postBody.innerText = item.body;
+				post.classList.add('post-body');
+
+				const postsContainer = document.getElementById('post-container');
+				// TODO: add <a> element instead of <button>, href will be './editPost/edit-post.html?postId=${postId}';
+				const editPostButton = document.createElement('button');
+				editPostButton.textContent = 'Edit Post';
+				editPostButton.onclick = () => updatePost(item.id);
+
+				post.appendChild(postTitle);
+				post.appendChild(postBody);
+				post.appendChild(editPostButton);
+
+				postsContainer.appendChild(post);
+			})
+		);
 }
 
-function getPostById() {
+function getPostById() {}
 
+function createPost(event) {
+	event.preventDefault();
+	const titleInput = document.getElementById('post-title-input');
+	const bodyInput = document.getElementById('post-body-input');
+	fetch('https://jsonplaceholder.typicode.com/posts', {
+		method: 'POST',
+		body: JSON.stringify({
+			title: titleInput.value,
+			body: bodyInput.value,
+			userId: 5,
+		}),
+		headers: {
+			'Content-type': 'application/json; charset=UTF-8',
+		},
+	})
+		.then((response) => response.json())
+		.then((json) => console.log(json));
 }
 
-function createPost() {
+function updatePost(postId) {
+	fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`, {
+		method: 'PUT',
+		body: JSON.stringify({
+			title: 'Hello',
+			body: 'World',
+			userId: 5,
+		}),
+		headers: {
+			'Content-type': 'application/json; charset=UTF-8',
+		},
+	})
+		.then((response) => response.json())
+		.then((json) => console.log(json));
 }
 
-function updatePost() {
-}
+function deletePost() {}
 
-function deletePost() {
-}
+// TODO: move all the code related to create post to another JS file
+document
+	.getElementById('post-form')
+	.addEventListener('submit', (event) => createPost(event));
