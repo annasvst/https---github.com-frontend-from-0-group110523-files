ed67cf0129d81f6a033a38e8c885f6f3d9f66ff6
Anna S. A <anna.atalay@minna.tech>
2023-12-20 22:22:11 +0100

    Add Lesson28 files

--- Lesson28/script.js
+++ Lesson28/script.js
@@ -20,7 +20,7 @@ const myPromise = new Promise((resolve, reject) => {
     const randomValue = Math.random(); // Generate a random number
 
     // Simulate success (resolve) if the random number is greater than 0.5
-    if (randomValue > 0.5) {
+    if (randomValue > 0.1) {
       resolve(`Success! Random Value: ${randomValue}`);
     } else {
       // Simulate an error (reject) if the random number is 0.5 or less
@@ -29,23 +29,87 @@ const myPromise = new Promise((resolve, reject) => {
   }, 2000); // Delay for 2 seconds (2000 milliseconds)
 });
 
+const myPromise2 = new Promise((resolve, reject) => {
+  // Inside the Promise, you can place the code you want to execute asynchronously.
+
+  // Simulate a delay using setTimeout
+  setTimeout(() => {
+    const randomValue = Math.random(); // Generate a random number
+
+    // Simulate success (resolve) if the random number is greater than 0.5
+    if (randomValue > 0.5) {
+      resolve(`Success! Random Value: ${randomValue}`);
+    } else {
+      // Simulate an error (reject) if the random number is 0.5 or less
+      reject(`Error! Random Value: ${randomValue}`);
+    }
+  }, 1000); // Delay for 2 seconds (2000 milliseconds)
+});
+
 // Step 2: Using .then() to handle resolved Promise
-myPromise
-  .then((result) => {
-    console.log(result); // This code will execute if the Promise is resolved
-  })
-  .catch((error) => {
-    console.error(error); // This code will execute if the Promise is rejected
-  });
+// myPromise
+//   .then((result) => {
+//     console.log(result); // This code will execute if the Promise is resolved
+//   })
+//   .catch((error) => {
+//     console.error(error); // This code will execute if the Promise is rejected
+//   });
 
 console.log("Promise created. Waiting for it to resolve or reject...");
 
 
 /* Asyc / await syntax for working with Promises */
 
+// const anyPromise = Promise.any([myPromise, myPromise2]); // checks if at least one promise is successfull 
+const firstPromise = Promise.race([myPromise, myPromise2]);// Returns result of the promise that was complited first
+// const allPromises = Promise.all([myPromise, myPromise]); // If you need to check all results
+
+// Arrow function
+// const promiseResult = async () => {
+//   try {
+//     const success = await(anyPromise);
+//     console.log('promiseResult function: ', success);
+//   } catch(error) {
+//     console.log(error);
+//   }
+// };
+
+// promiseResult();
+
+
 // Examples of making a function asyncronous
 // Function declaration
+async function getFirstResult () {
+  try {
+    const success = await(firstPromise);
+
+    // success.map(async (successItem) => {
+    //     const result = await(myRequest);
+    // })
+    
+    console.log('getFirstResult function: ', success);
+  } catch(error) {
+    console.log(error);
+  } finally {
+    console.log('Final code block!');
+  }
+};
+
+getFirstResult();
+
 // Function expression
-// Arrow function
+// TODO: Anna to check what error case is not handled
+// const allPromisesResult = async function () {
+//   try {
+//     const success = await(allPromises);
+//     console.log('allPromisesResult function: ', success);
+//   } catch(error) {
+//     console.log(error);
+//   } finally {
+//     console.log('Final code block!');
+//   }
+// }
+
+// allPromisesResult();
 
 // Try, catch, finally
