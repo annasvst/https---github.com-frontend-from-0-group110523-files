b54dda7fb639e33013d4e826c03aa71d6bc084cf
Anna S. A <anna.atalay@minna.tech>
2024-01-03 20:11:26 +0100

    add lesson 30 completed files

--- Lesson30/todo-app/src/App.js
+++ Lesson30/todo-app/src/App.js
@@ -1,10 +1,27 @@
 import './App.css';
+import { List } from './shared-components/List';
+import { Card } from './shared-components/Card';
+import { Demo } from './shared-components/Demo';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
-}
+/* 1. {} for JS code in JSX
+  2. Attribute names can vary (e.g. class is className, onclick is onClick)
+  3. JSX expressions must have one parent element.
+*/
 
-export default App;
+export const App = () => {
+	// JSX = JavaScript XML
+	return (
+		<div className='app'>
+			<header>Header</header>
+			<main>
+				<Card>
+					<Demo />
+				</Card>
+				<Card>
+					<List />
+				</Card>
+			</main>
+			<footer>Footer of the page</footer>
+		</div>
+	);
+};
