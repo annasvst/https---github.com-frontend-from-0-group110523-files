1bfd0d8f46313a802bcef72c96b32dbaa6e3211b
Anna S. A <anna.atalay@minna.tech>
2024-01-10 22:15:58 +0300

    Add lesson31 completed files

--- Lesson31/todo-app/src/App.js
+++ Lesson31/todo-app/src/App.js
@@ -1,14 +1,22 @@
 import './App.css';
 import { List } from './shared-components/List';
 import { Card } from './shared-components/Card';
-import { Demo } from './shared-components/Demo';
+import { Ex1 } from './exercise-components/Exercises1';
+import { Ex2 } from './exercise-components/Exercise2';
+import { Ex3 } from './exercise-components/Exercise3';
 
 export const App = () => {
 	return (
 		<div className='app'>
 			<main>
 				<Card>
-					<Demo />
+					<Ex1/>
+				</Card>
+				<Card>
+					<Ex2/>
+				</Card>
+				<Card>
+					<Ex3/>
 				</Card>
 				<Card>
 					<List />
