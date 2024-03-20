459b285fb8821300ede7991959ccac15ac0ee58d
Anna S. A <anna.atalay@minna.tech>
2024-03-20 18:26:17 +0100

    Add Lesson41 starter files

---
+++ Lesson41/my-app/src/components/CssModulesExample/index.jsx
@@ -0,0 +1,9 @@
+
+export const CssModulesExample = () => {
+  return (
+    <div>
+      <h1>This is CSS Modules Example component</h1>
+      <p>A step up from regular CSS. With CSS Modules, you still write CSS in separate files, but when you import them into your React component, they're scoped to that component only. This means no more worrying about class name conflicts across your app!</p>
+    </div>
+  );
+};
---
+++ Lesson41/my-app/src/components/StyledComponentsExample/index.jsx
@@ -0,0 +1,10 @@
+import Typography from '@mui/material/Typography';
+
+export const StyledComponentsExample = () => {
+  return (
+    <div>
+      <h1>This is Styled Components Example component</h1>
+      <Typography variant='body1'>If you're into the JavaScript-in-CSS vibe, this is for you. Styled components let you write actual CSS in your JavaScript file, using tagged template literals. It's like merging styles and components into one, which can be really powerful for component-based designs.</Typography>
+    </div>
+  );
+};
